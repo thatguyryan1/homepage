5261e5ceb1bd0da362395a31172644876713f749
thatguyryan1 <redaughtry@verizon.net>
2023-12-11 17:58:14 -0500

    sdfadsf
    asdfasdf

--- dictionary/scripts.js
+++ dictionary/scripts.js
@@ -1,16 +1,3 @@
-project.currentStyle = {
-	fillColor: 'black'
-};
-
-<img id="myImg" src="images/back-static.png" />
- 
-$(function() {
-    $("#myImg").hover(
-        function() {
-            $(this).attr("src", "images/back-ani.gif");
-        },
-        function() {
-            $(this).attr("src", "images/back-static.png");
-        }                         
-    );                  
-});
+$( function() {
+    $(".draggable").draggable();
+})
